c6465168792de4a8e8dd74d3ff753e4fe0ddbda1
ACER <ACER@DESKTOP-E1E9EHH.gateway>
2019-02-03 23:05:11 +1100

    hello world output - 0302

---
+++ Jiten new/src/Newpackage/Demoworld.java
@@ -0,0 +1,12 @@
+package Newpackage;
+
+public class Demoworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
