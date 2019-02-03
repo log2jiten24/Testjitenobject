da1fda27bf52076a453d432817ad664b8334d729
ACER <ACER@DESKTOP-E1E9EHH.gateway>
2019-02-03 23:10:25 +1100

    commit - 0302

--- Jiten new/src/Newpackage/Demoworld.java
+++ Jiten new/src/Newpackage/Demoworld.java
@@ -5,6 +5,7 @@ public class Demoworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str1 = "Jiten" ;
+		System.out.println("hello :"+str1);
 		System.out.println("Hello world");
 
 	}
