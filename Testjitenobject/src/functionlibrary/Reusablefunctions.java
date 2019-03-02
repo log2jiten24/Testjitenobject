46df22efb545d1b1c108073575d62aaa7c5af676
Kumar Jitendra <log2jeet24@gmail.com>
2019-03-03 03:21:52 +1100

    commit - 03mar

---
+++ Testjitenobject/src/functionlibrary/Reusablefunctions.java
@@ -0,0 +1,31 @@
+package functionlibrary;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.tools.ant.util.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import com.google.common.io.Files;
+public class Reusablefunctions {
+
+	public static void capturescreenshot (WebDriver driver ,String screenshotname)
+	{
+		try {
+		//TakesScreenshot is an interface ,we are typecasting interface to create object
+		TakesScreenshot ts = (TakesScreenshot)driver  ;
+		//getScreenshotAs - method to take screenshot and store in the memory
+		File src = ts.getScreenshotAs(OutputType.FILE) ;
+		//store the source file into the destination
+		
+		Files.copy(src, new File("./Screenshots/+ "+screenshotname+".png"));
+		
+		System.out.println ("Screenshots taken");
+		} catch (Exception e) 
+		{
+		System.out.println("Screenshot failure :" + e.getMessage());
+		}
+	}
+}
